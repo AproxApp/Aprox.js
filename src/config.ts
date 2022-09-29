511b6fe902e4adcc8ad33a27149b3ca9da10346b
BuildTools <unconfigured@null.spigotmc.org>
2022-09-29 17:43:39 +0200

    Aprox.js

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const LIBRARY_VERSION = "6.0.17";
+
+export const defaultConfig = {
+    apiURL: "https://api.aprox.app",
+    autoReconnect: true,
+    heartbeat: 30,
+    debug: false,
+
+    cache: false,
+    unreads: false,
+
+    ackRateLimiter: true,
+};
